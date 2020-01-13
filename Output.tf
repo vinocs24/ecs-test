0f8032846ba9fc53c09bbf8f56d93081a94bb06a
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-13 13:55:50 +0530

    Create Output.tf

---
+++ Output.tf
@@ -0,0 +1,15 @@
+output "instance_security_group" {
+  value = aws_security_group.ecs-securitygroup.id
+}
+
+output "launch_configuration" {
+  value = aws_launch_configuration.ecs-test-launchconfig.id
+}
+
+output "asg_name" {
+  value = aws_autoscaling_group.ecs-test-autoscaling.id
+}
+
+output "elb_hostname" {
+  value = aws_alb.main.dns_name
+}
