930bc28cccf7c5d6e71b342553196e182f392236
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-14 12:25:21 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -332,7 +332,7 @@ resource "aws_ecs_service" "test" {
     #target_group_arn = aws_alb_target_group.test.id
     #container_name   = "ghost"
     #container_port   = "2368"
-    elb_name = aws_elb.test.id
+    elb_name = aws_alb.main.id
     container_name = "test-http"
     container_port = 8080
     
