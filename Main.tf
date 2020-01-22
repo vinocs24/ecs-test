6f0bc9921ecc3306d119aa7cebfafe0289b575a5
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-22 17:05:12 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,7 +1,7 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "ecs-test-1990"
+    bucket = "ecs-test-19900624"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
